c134ddbe63168c2cf42cfe57d3f99648766c1683
t03jam8 <grovejam@gmail.com>
2019-11-23 14:44:29 +0100

    Still issues but keeping the reset stuff

--- utils/handleWd.R
+++ utils/handleWd.R
@@ -1,5 +1,7 @@
 # A function to handle the setting of working dirs 
+source('./utils/resetWd.R')
 handleWd <- function(localDir = '') {
+  resetWd()
   firstChar <- substr(localDir, 1, 1)
   if(firstChar != '/'){
     warning("There is no forward slash!")
--- utils/resetWd.R
+++ utils/resetWd.R
@@ -2,4 +2,3 @@ resetWd <- function(){
   root <- "/Users/jamesmargrove/Documents/analysis/water-inundation-shapes-species-distributions-in-tropical-forests/"
   setwd(root)
 }
-
