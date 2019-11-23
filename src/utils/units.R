dd84cfc37b205e79813124fb18d204718fa59cb2
t03jam8 <grovejam@gmail.com>
2019-11-23 19:12:56 +0100

    Added in some usefull units

--- src/utils/units.R
+++ src/utils/units.R
@@ -1,3 +1,24 @@
-# values for making publication tables 
+# Greek letters 
 mu <- "μ"
 sigma <- "σ"
+
+# Units 
+CM <- "cm"
+G <- "g"
+HA <- "ha"
+DEG <- "\U00B0"
+
+# Operators 
+NEG <- "\U207B"
+POS <- "\U207A"
+
+# Superscript numbers 
+ZERO <- "\U2070"
+ONE <- "\U00B9"
+TWO <- "\U00B2"
+THREE <- "\U00B3"
+
+
+# Usefull combinations 
+PER_HA <- paste(HA, NEG, ONE, sep = "")
+PER_CM3 <- paste(CM, NEG, THREE, sep = "")
