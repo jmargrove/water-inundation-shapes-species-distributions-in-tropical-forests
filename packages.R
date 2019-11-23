fb2473a519e3643d18a83d3d5c50f903be863ac6
t03jam8 <grovejam@gmail.com>
2019-11-24 00:11:02 +0100

    Writting and installing packages

--- packages.R
+++ packages.R
@@ -1,1 +1,15 @@
-install.packages("lintr")
+# install these packages
+install.packages("INLA", repos=c(getOption("repos"), INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
+
+checkAndInstallPackages = function(){
+  if("lme4" %in% rownames(installed.packages())){
+    print("install packages")
+  } else {
+    print("allready has package")
+  }
+}
+
+checkAndInstallPackages()
+
+
+
