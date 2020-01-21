4d7b37978d3e789e17497cd3b63dbb3ed8b1a75f
t03jam8 <grovejam@gmail.com>
2020-01-21 22:21:48 +0100

    Renamed the plots

--- src/figures/graph.fig3.R
+++ src/figures/graph.fig3.R
@@ -1,9 +1,12 @@
-# combining the result figure 2
+# combining the result figure 3 
 rm(list = ls())
 loadPlots <- function(){
-  panel_a <- source('./src/analysis/inla-spde-species-distributions/species-distribution.graph.R')$value 
-  panel_b <- source('./src/analysis/sepilok-field-experiment/mortality-exp.graph.R')$value 
-  return(list(a = panel_a, b = panel_b))
+  panel_a <- source('./src/analysis/elevation-predicted-by-density+inundation/riskratio.graph.R')$value 
+  
+  panel_b <- source('./src/analysis/elevation-predicted-by-density+inundation/density.graph.R')$value 
+  panel_c <- source('./src/analysis/ontogenic-wood-density-change/ontogenic-density.graph.R')$value # the triangle
+  panel_d <-  source('./src/analysis/nursery-experiment/final-wood-density.graph.R')$value 
+  return(list(a = panel_a, b = panel_b, c = panel_c, d = panel_d))
 }
 
 
@@ -17,10 +20,10 @@ library(ggpubr) # package to organise the plots
 b <- ggarrange(
   plots$a$plot,
   plots$b$plot,
-  ncol = 1, 
-  nrow = 2, 
+  plots$c$plot,
+  plots$d$plot,
   # adding the labels 
-  labels = c('a', 'b'),
+  labels = c('a', 'b', 'c', 'd'),
   # styling the labels 
   font.label = list(size = 14, face = "bold", family = gg_theme$font_family) 
 )
@@ -28,5 +31,6 @@ b <- ggarrange(
 dims <- (209.9 - (25.4 * 2)) # full width of A4 * 2
 
 # save the panel plot for fig 3
-ggsave(b, file ='./src/figures/results.fig2.png', width = dims, height = dims, units = "mm")
+ggsave(b, file ='./src/figures/graph.fig3.png', width = dims, height = dims, units = "mm")
+
 
