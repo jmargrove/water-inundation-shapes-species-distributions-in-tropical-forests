d48447790c9292a36b4f2d2fdb417187710bbdc9
t03jam8 <grovejam@gmail.com>
2020-01-22 23:29:26 +0100

    Styling the wood density plot

--- src/analysis/wood-density-elevation-distribution/wood-density-elevation-prediction.graph.R
+++ src/analysis/wood-density-elevation-distribution/wood-density-elevation-prediction.graph.R
@@ -11,23 +11,24 @@
 
     # colors 
     theme <- source("./src/utils/theme.R")$value
+    gg_theme <- source("./src/utils/gg-theme.graph.R")$value
 
     # Plot of the results 
     p1 <- ggplot(gls_analysis$preds, aes(x = density_mean, y = elevation_mean)) +
-      geom_point(data = data, aes(x = density_mean, y = elevation_mean), alpha = 0.5) +
-      geom_line() +
-      theme_bw() +
-      geom_ribbon(aes(ymin = CI025, ymax = CI975), alpha = 0.5) +
-      ylab("Elevation (m asl)") +
+      geom_point(data = data, aes(x = density_mean, y = elevation_mean), alpha = 0.5, size = 0.5) +
+      geom_line(size = 0.5) +
+      geom_ribbon(aes(ymin = CI025, ymax = CI975), alpha = gg_theme$ribbon_alpha, color = gg_theme$ribbon_color ) +
+      ylab("Elevation (m) asl") +
       xlab(bquote("Adult wood density g" ~ cm ^ -3)) +
       theme(legend.position = "top") +
       scale_fill_manual(values = theme$selectColorPalette()) +
       xlim(NA, 0.62) +
       ylim(NA, 140) +
-      theme(text = element_text(size = 20)) +
       geom_line(data = qr_analysis$preds[qr_analysis$preds$Quantile != "0.5",], # except the median 
-                aes(x = density_mean, y = elevation_mean, color = Quantile), linetype = 1, size = 1.5) +
-      scale_color_manual(values = theme$selectColorPalette()) # color the lines
+                aes(x = density_mean, y = elevation_mean, color = Quantile), linetype = 1, size = 0.5) +
+      scale_color_manual(values = theme$selectColorPalette()) + # color the lines
+      gg_theme$t
+    
 
     return(
       list(
