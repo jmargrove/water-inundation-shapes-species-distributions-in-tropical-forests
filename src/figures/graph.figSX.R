a3c75ec5ec01991a42b4d3219a0485b78b5206c4
t03jam8 <grovejam@gmail.com>
2020-01-22 23:29:06 +0100

    Adding in figs2

--- src/figures/graph.figSX.R
+++ src/figures/graph.figSX.R
@@ -6,3 +6,6 @@ plot <- source("./src/analysis/wood-density-elevation-distribution/wood-density-
 plot$qr_analysis$coefs[c(2, 10), ]
  
 # the plot 
+# save the panel plot for fig 3
+ggsave(plot$plot, file ='./src/figures/graph.figS2.png', width = dims, height = dims / 2, units = "mm")
+
