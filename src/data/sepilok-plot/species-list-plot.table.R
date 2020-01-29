62021395e71645d00221fe592b98b4baf146ae72
t03jam8 <grovejam@gmail.com>
2020-01-29 11:44:00 +0100

    Updating the species list creation

--- src/data/sepilok-plot/species-list-plot.table.R
+++ src/data/sepilok-plot/species-list-plot.table.R
@@ -1,13 +1,12 @@
-rm(list = ls())
-# Import the table data
-data <- read.csv('./src/data/sepilok-160ha-plot.raw.csv')
-str(data)
-
-# set up table with species values
-table <- data.frame(species = as.character(levels(data$species)))
-table$sp <- as.character(levels(data$sp))
-
-write.csv(table, file = "./src/summary-tables/species-list-plot.table.csv", row.names = FALSE)
-
+# Species list for the 160ha forest plot 
+(function(){
+  # Import the table data
+  data <- read.csv('./src/data/sepilok-plot/sepilok-160ha-plot.raw.csv')
+  # Set up table with species values
+  table <- data.frame(species = as.character(levels(data$species)))
+  table$sp <- as.character(levels(data$sp))
+  #write.csv(table, file = "./src/summary-tables/species-list-plot.table.csv", row.names = FALSE)
+  return(table)
+})()
 
 
